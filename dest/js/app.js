cdeaeba24b037c8a98af745871e6ac00cb508f39
MrHidalgo <Luckyhidalgo@gmail.com>
2019-10-15 10:54:15 +0300

    Create Affiliate app account inside

--- dest/js/app.js
+++ dest/js/app.js
@@ -1087,6 +1087,34 @@ $(document).ready(function (ev) {
     });
   };
 
+  var initAffiliate = function initAffiliate() {
+    var initCopyToClipboard = function initCopyToClipboard(str) {
+      var el = document.createElement('textarea');
+
+      el.value = str;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+
+      document.body.appendChild(el);
+
+      var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+
+      if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+      }
+    };
+
+    $('.affiliate-inside__input-btn').on('click', function (ev) {
+      initCopyToClipboard($(ev.currentTarget).prev('input').val());
+    });
+  };
+
   /**
    * @description Init all method
    */
@@ -1119,6 +1147,7 @@ $(document).ready(function (ev) {
     initPricingTabs();
 
     initStickyElem();
+    initAffiliate();
   };
   initJquery();
 });
